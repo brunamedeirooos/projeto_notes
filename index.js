36e6e5e2085078e2c1c17c460a641de3881a2964
bruna medeiros <brunamedeirosfelix@gmail.com>
2023-02-28 13:42:32 -0300

    criado views

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//configurações - chamando os pacotes que foram instalados
+//configurações - importando os pacotes que foram instalados
 
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -7,11 +7,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
+//template engine - chamando para criar um html dinamico
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`projeto rodando na porta: ${port}`);
 } )
 
-//rotas - basicamente é a url que acessamos
+//rotas - basicamente é a url que acessamos - chamando a pagina view home.handlebars
 app.get('/', function(req, res){
-    res.send('o aplicativo está funcionando')
+    res.render('home')
 })
